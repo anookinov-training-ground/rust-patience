89a0e9e46da009f370a058cfe891a678a75ddf00
Alexander Nookinov <anookinov@gmail.com>
2021-11-04 09:36:26 +0700

    add explanation of async await and futures

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,46 @@ use std::future::Future;
 fn main() {
     println!("Hello, world!");
 
-    let x = foo1();
+    let read_from_terminal = std::thread::spawn(move || {
+        let x = std::io::Stdin::lock(std::io::stdin());
+        for line in x.lines() {
+            // do something on user input
+        }
+    });
+
+    let read_from_network = std::thread::spawn(move || {
+        let mut x = std::net::TcpListener::bind("0.0.0.0:8080").unwrap();
+        while let Ok(stream) = x.accept() {
+            // do something on stream
+            let handle = std::thread::spawn(move || {
+                handle_connection(stream);
+            });
+        }
+    });
+
+    let network = read_from_network();
+    let terminal = read_from_terminal();
+    let mut foo = foo2();
+
+    loop {
+        select! {
+            stream <- network.await => {
+                // do something on stream
+            }
+            line <- terminal.await => {
+                // do something with line
+            }
+            foo <- foo.await => {
+
+            }
+        };
+    }
+
+    // let x = foo2();
 }
+
+async fn read_to_string(s: &str) {}
+fn expensive_function(_: ()) {}
 
 async fn foo1() -> usize {
     println!("foo");
@@ -15,7 +53,37 @@ async fn foo1() -> usize {
 
 fn foo2() -> impl Future<Output = usize> {
     async {
-        println!("foo");
+
+        // let x = read_to_string("file").await;
+
+        let fut = read_to_string("file");
+        let x = loop {
+            if let Some(result) = fut.try_check_completed() {
+                break result;
+            } else {
+                fut.try_make_progress();
+                yield;
+            }
+        }
+
+        // First time:
+        println!("foo1");
+        read_to_string("file1").await;
+        // let fut = read_to_string("file1"); // Wait here
+        // while !fut.is_ready() {
+        //     std::thread::yield_now();
+        //     fut.try_complete();
+        // }
+        // let result = fut.take_result();
+        // Second time:
+        println!("foo1");
+        read_to_string("file2").await; // Wait here
+        println!("foo1");
+        let x = /* waiting on */ read_to_string("file3").await;
+        println!("foo1");
+        expensive_function(x);
+        /* yield again and wait on */ read_to_string("file4").await;
+        println!("foo2");
         0
     }
 }
