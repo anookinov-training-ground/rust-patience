62e30a582658c09ab00da7bde74ca28dd80fcfa4
Alexander Nookinov <anookinov@gmail.com>
2021-11-04 09:02:52 +0700

    show async functions into 2 forms

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![allow(dead_code, unused_variables)]
+
+use std::future::Future;
+
 fn main() {
     println!("Hello, world!");
+
+    let x = foo1();
+}
+
+async fn foo1() -> usize {
+    println!("foo");
+    0
+}
+
+fn foo2() -> impl Future<Output = usize> {
+    async {
+        println!("foo");
+        0
+    }
 }
